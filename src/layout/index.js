5b2028f4f28d6e30ffb6691918d6dd08a02e527e
liangshuai <526995681@qq.com>
2019-12-26 15:52:00 +0800

    样式√

--- src/layout/index.js
+++ src/layout/index.js
@@ -4,7 +4,7 @@ import './index.css'
 export default function index(props) {
     return (
         <div>
-            <NavLink to='/'>首页</NavLink>
+            <NavLink exact to='/'>首页</NavLink>
             <NavLink to='/welcome'>欢迎页</NavLink>
             <NavLink to='/login'>登录页</NavLink>
             <NavLink to='/counter'>计数器</NavLink>
