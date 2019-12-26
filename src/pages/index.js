00981508a6f97ff74fdefb7af4678acdd9a641e9
liangshuai <526995681@qq.com>
2019-12-26 18:43:31 +0800

    好气√

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { } from '../services/student'
 
-export default function index() {
-    return (
-        <div>
-
-        </div>
-    )
+export default function Index() {
+  <h1></h1>
+  return (
+    <h1>
+      {ds}
+    </h1>
+  )
 }
